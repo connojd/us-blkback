a50a505d01fb12415ccdd0f3051669299ef17a16
Brendan Kerrigan <kerriganb@ainfosec.com>
2020-07-30 07:06:26 -0400

    Format the disk image unit test
    Signed-off-by: Brendan Kerrigan <kerriganb@ainfosec.com>

--- disk-image-test.cpp
+++ disk-image-test.cpp
@@ -11,131 +11,139 @@ DiskImage di("./test.img");
 
 
 TEST_CASE("Test Create Backing File", "[backingFile]"){
-SECTION("Checking if ./test.img exists"){
-// create test.img
-const std::string& name = "./test.img";
-struct stat buffer;
-REQUIRE(stat (name.c_str(), &buffer) == 0); // determine if the file exists
-}
+    SECTION("Checking if ./test.img exists"){
+        // create test.img
+        const std::string& name = "./test.img";
+        struct stat buffer;
+        REQUIRE(stat (name.c_str(), &buffer) == 0); // determine if the file exists
+    }
 }
 
 TEST_CASE("Test Read Sector","[readSector]"){
-SECTION("Checking read Sector Function"){
-std::vector<char> ff(512, 0xff);
-std::vector<char> empty(512, 0x00);
+    SECTION("Checking read Sector Function"){
+        std::vector<char> ff(512, 0xff);
+        std::vector<char> empty(512, 0x00);
 
-rc = di.readSector(512, empty);
-REQUIRE(rc == 0);
+        rc = di.readSector(512, empty);
+        REQUIRE(rc == 0);
 
-rc = di.readSector(512, ff);
-REQUIRE(rc == 0);
+        rc = di.readSector(512, ff);
+        REQUIRE(rc == 0);
 
-int16_t tooHigh = 10000;
-rc = di.readSector(tooHigh, empty);
-REQUIRE(rc == -1);
+        int16_t tooHigh = 10000;
+        rc = di.readSector(tooHigh, empty);
+        REQUIRE(rc == -1);
 
-rc = di.readSector(tooHigh, ff);
-REQUIRE(rc == -1);
-}
+        rc = di.readSector(tooHigh, ff);
+        REQUIRE(rc == -1);
+    }
 }
 
 TEST_CASE("Test Write Sector","[readSector]"){
-SECTION("Checking Write Sector"){
-std::vector<char> ff(512, 0xff);
-std::vector<char> empty(512, 0x00);
+    SECTION("Checking Write Sector"){
+        std::vector<char> ff(512, 0xff);
+        std::vector<char> empty(512, 0x00);
 
-// WRITE SECTOR
-rc = di.writeSector(512, ff);
-REQUIRE(rc == 0);
+        // WRITE SECTOR
+        rc = di.writeSector(512, ff);
+        REQUIRE(rc == 0);
 
-rc = di.writeSector(256, ff);
-REQUIRE(rc == 0);
+        rc = di.writeSector(256, ff);
+        REQUIRE(rc == 0);
 
-rc = di.writeSector(256, ff);
-REQUIRE(rc == 0);
+        rc = di.writeSector(256, ff);
+        REQUIRE(rc == 0);
 
-int16_t tooHigh = 10000;
-rc = di.writeSector(tooHigh, ff);
-REQUIRE(rc == -1);
-}
+        int16_t tooHigh = 10000;
+        rc = di.writeSector(tooHigh, ff);
+        REQUIRE(rc == -1);
+    }
 }
 
 TEST_CASE("Sector Read/Write is Bounded Correctly", "[writeSector]"){
-SECTION("Sector Write Bounding"){
-std::vector<char> buf1(512,0xAA);
-std::vector<char> buf2(512, 0xBB);
-std::vector<char> buf3(512,0xCC);
-std::vector<char> buf4(512,0xDD);
-std::vector<char> buf5(512,0xEE);
-std::vector<char> buf6(512,0xFF);
+    SECTION("Sector Write Bounding"){
+        std::vector<char> buf1(512,0xAA);
+        std::vector<char> buf2(512, 0xBB);
+        std::vector<char> buf3(512,0xCC);
+        std::vector<char> buf4(512,0xDD);
+        std::vector<char> buf5(512,0xEE);
+        std::vector<char> buf6(512,0xFF);
 
-blkif_sector_t sectNum = 512;
-std::vector<char> empty(512, 0x00);
-std::vector<char> ff(512, 0xff);
+        blkif_sector_t sectNum = 512;
+        std::vector<char> empty(512, 0x00);
+        std::vector<char> ff(512, 0xff);
 
-//511
-rc = di.writeSector(sectNum-1, empty);
-rc = di.readSector(sectNum-1,buf1);
-REQUIRE(std::equal(empty.begin(),empty.end(), buf1.begin()) == true);
+        //511
+        rc = di.writeSector(sectNum-1, empty);
+        rc = di.readSector(sectNum-1,buf1);
+        REQUIRE(std::equal(empty.begin(),empty.end(), buf1.begin()) == true);
 
-//512
-rc = di.writeSector(sectNum, ff);
-rc = di.readSector(sectNum, buf2);
-REQUIRE(std::equal(ff.begin(), ff.end(), buf2.begin()) == true);
+        //512
+        rc = di.writeSector(sectNum, ff);
+        rc = di.readSector(sectNum, buf2);
+        REQUIRE(std::equal(ff.begin(), ff.end(), buf2.begin()) == true);
 
-//513
-rc = di.writeSector(sectNum+1, empty);
-rc = di.readSector(sectNum+1, buf3);
-REQUIRE(std::equal(empty.begin(), empty.end(),buf3.begin())==true);
+        //513
+        rc = di.writeSector(sectNum+1, empty);
+        rc = di.readSector(sectNum+1, buf3);
+        REQUIRE(std::equal(empty.begin(), empty.end(),buf3.begin())==true);
 
-// reverse order 513 -> 512 -> 511
-//513
-rc = di.writeSector(sectNum+1, empty);
-rc = di.readSector(sectNum+1, buf4);
-REQUIRE(std::equal(empty.begin(), empty.end(),buf4.begin())==true);
+        // reverse order 513 -> 512 -> 511
+        //513
+        rc = di.writeSector(sectNum+1, empty);
+        rc = di.readSector(sectNum+1, buf4);
+        REQUIRE(std::equal(empty.begin(), empty.end(),buf4.begin())==true);
 
-// 512
-rc = di.writeSector(sectNum, ff);
-rc = di.readSector(sectNum, buf5);
-REQUIRE(std::equal(ff.begin(), ff.end(), buf5.begin()) == true);
+        // 512
+        rc = di.writeSector(sectNum, ff);
+        rc = di.readSector(sectNum, buf5);
+        REQUIRE(std::equal(ff.begin(), ff.end(), buf5.begin()) == true);
 
-//511
-rc = di.writeSector(sectNum, empty);
-rc = di.readSector(sectNum, buf6);
-REQUIRE(std::equal(empty.begin(), empty.end(), buf6.begin()) == true);
-}
-SECTION("Testing zero and edge cases"){
-blkif_sector_t sectNum = 512;
-std::vector<char> empty(512, 0x00);
-std::vector<char> ff(512, 0xff);
+        //511
+        rc = di.writeSector(sectNum, empty);
+        rc = di.readSector(sectNum, buf6);
+        REQUIRE(std::equal(empty.begin(), empty.end(), buf6.begin()) == true);
+    }
+    SECTION("Testing zero and edge cases"){
+        blkif_sector_t sectNum = 512;
+        std::vector<char> empty(512, 0x00);
+        std::vector<char> ff(512, 0xff);
 
-//large sector cases
-// large sectNumber
-std::vector<char> buf7(512,0x11);
-std::vector<char> buf8(512,0x22);
-std::vector<char> buf9(512,0x33);
-std::vector<char> buf10(512,0x44);
+        //large sector cases
+        // large sectNumber
+        std::vector<char> buf7(512,0x11);
+        std::vector<char> buf8(512,0x22);
+        std::vector<char> buf9(512,0x33);
+        std::vector<char> buf10(512,0x44);
 
-//512*2 -> 2024
-//ff
-rc = di.writeSector(sectNum*2, ff);
-rc = di.readSector(sectNum*2, buf7);
-REQUIRE(std::equal(ff.begin(), ff.end(), buf7.begin()) == true);
-// empty
-rc = di.writeSector(sectNum*2, empty); //512*2
-rc = di.readSector(sectNum*2, buf8);
-REQUIRE(std::equal(empty.begin(), empty.end(), buf8.begin()) == true);
+        //512*2 -> 2024
+        //ff
+        rc = di.writeSector(sectNum*2-1, ff);
+        REQUIRE(rc == 0);
+        rc = di.readSector(sectNum*2-1, buf7);
+        REQUIRE(rc == 0);
+        REQUIRE(std::equal(ff.begin(), ff.end(), buf7.begin()) == true);
+        // empty
+        rc = di.writeSector(sectNum*2 - 1, empty); //512*2
+        REQUIRE(rc == 0);
+        rc = di.readSector(sectNum*2 - 1, buf8);
+        REQUIRE(rc == 0);
+        REQUIRE(std::equal(empty.begin(), empty.end(), buf8.begin()) == true);
 
-//zero case
-//ff
-rc = di.writeSector(0, ff);
-rc = di.readSector(0, buf9);
-REQUIRE(std::equal(ff.begin(), ff.end(), buf7.begin()) == true);
-// empty
-rc = di.writeSector(0, empty); //512*2
-rc = di.readSector(0, buf10);
-REQUIRE(std::equal(empty.begin(), empty.end(), buf8.begin()) == true);
-};
+        //zero case
+        //ff
+        rc = di.writeSector(0, ff);
+        REQUIRE(rc == 0);
+        rc = di.readSector(0, buf9);
+        REQUIRE(rc == 0);
+        REQUIRE(std::equal(ff.begin(), ff.end(), buf7.begin()) == true);
+        // empty
+        rc = di.writeSector(0, empty); //512*2
+        REQUIRE(rc == 0);
+        rc = di.readSector(0, buf10);
+        REQUIRE(rc == 0);
+        REQUIRE(std::equal(empty.begin(), empty.end(), buf8.begin()) == true);
+    };
 }
 
 // todo: at this time this is not necessary
